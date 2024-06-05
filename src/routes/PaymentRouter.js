f066da802be5dc9e033b8094ece5aea573943d5c
huycao2710 <caovanhuy2710@gmail.com>
2024-06-06 01:08:29 +0700

    update zalopay và cho cút vnpay

--- src/routes/PaymentRouter.js
+++ src/routes/PaymentRouter.js
@@ -11,4 +11,66 @@ router.get('/config', (req, res) => {
     })
 })
 
+const config = {
+    app_id: "2554",
+    key1: "sdngKKJmqEMzvh5QQcdD2A9XBSKUNaYn",
+    key2: "trMrHtvjo6myautxDUiAcYsVtaeQ8nhf",
+    endpoint: "https://sb-openapi.zalopay.vn/v2/create"
+};
+
+router.post('/zalopay', async (req, res) => {
+    const embed_data = {};
+
+    const items = [{}];
+    const transID = Math.floor(Math.random() * 1000000);
+    const order = {
+        app_id: config.app_id,
+        app_trans_id: `${moment().format('YYMMDD')}_${transID}`,
+        app_user: "user123",
+        app_time: Date.now(),
+        item: JSON.stringify(items),
+        embed_data: JSON.stringify(embed_data),
+        amount: 50000,
+        description: `Lazada - Payment for the order #${transID}`,
+        bank_code: "",
+    };
+
+    const data = config.app_id + "|" + order.app_trans_id + "|" + order.app_user + "|" + order.amount + "|" + order.app_time + "|" + order.embed_data + "|" + order.item;
+    order.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
+
+    const result = await axios.post(config.endpoint, null, { params: order })
+    return res.status(200).json({
+        status: 'OK',
+        app_trans_id: order.app_trans_id,
+        data: result.data
+    })
+}
+)
+router.post('/check_zalopay', async (req, res) => {
+    const { trans_id } = req.body
+    let postData = {
+        app_id: config.app_id,
+        app_trans_id: trans_id,
+    }
+
+    let data = postData.app_id + "|" + postData.app_trans_id + "|" + config.key1;
+    postData.mac = CryptoJS.HmacSHA256(data, config.key1).toString();
+
+
+    let postConfig = {
+        method: 'post',
+        url: 'https://sb-openapi.zalopay.vn/v2/query',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        data: qs.stringify(postData)
+    };
+
+    const result = await axios(postConfig)
+    return res.status(200).json({
+        status: 'OK',
+        data: result.data
+    })
+})
+
 module.exports = router
