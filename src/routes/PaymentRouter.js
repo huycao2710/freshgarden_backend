c67ea8fd0c4517c7a5ac0740f1f1d5b097d1f28a
huycao2710 <caovanhuy2710@gmail.com>
2024-06-06 22:04:37 +0700

    update momo

--- src/routes/PaymentRouter.js
+++ src/routes/PaymentRouter.js
@@ -14,7 +14,7 @@ router.get('/config', (req, res) => {
         data: process.env.CLIENT_ID
     })
 })
-
+//zalopay
 const config = {
     app_id: "2554",
     key1: "sdngKKJmqEMzvh5QQcdD2A9XBSKUNaYn",
@@ -83,6 +83,101 @@ router.post('/check_zalopay', async (req, res) => {
         data: result.data
     })
 })
+//momo
+var accessKey = 'F8BBA842ECF85';
+var secretKey = 'K951B6PE1waDMi640xX08PD3vg6EkVlz';
+router.post('/momo', async (req, res) => {
+    var orderInfo = 'pay with MoMo';
+    var partnerCode = 'MOMO';
+    var redirectUrl = 'https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b';
+    var ipnUrl = 'https://webhook.site/b3088a6a-2d17-4f8d-a383-71389a6c600b';
+    var requestType = "payWithMethod";
+    var amount = '50000';
+    var orderId = partnerCode + new Date().getTime();
+    var requestId = orderId;
+    var extraData = '';
+    var orderGroupId = '';
+    var autoCapture = true;
+    var lang = 'vi';
 
+    //before sign HMAC SHA256 with format
+    //accessKey=$accessKey&amount=$amount&extraData=$extraData&ipnUrl=$ipnUrl&orderId=$orderId&orderInfo=$orderInfo&partnerCode=$partnerCode&redirectUrl=$redirectUrl&requestId=$requestId&requestType=$requestType
+    var rawSignature = "accessKey=" + accessKey + "&amount=" + amount + "&extraData=" + extraData + "&ipnUrl=" + ipnUrl + "&orderId=" + orderId + "&orderInfo=" + orderInfo + "&partnerCode=" + partnerCode + "&redirectUrl=" + redirectUrl + "&requestId=" + requestId + "&requestType=" + requestType;
+    //puts raw signature
+    console.log("--------------------RAW SIGNATURE----------------")
+    console.log(rawSignature)
+    //signature
+    const crypto = require('crypto');
+    var signature = crypto.createHmac('sha256', secretKey)
+        .update(rawSignature)
+        .digest('hex');
+    console.log("--------------------SIGNATURE----------------")
+    console.log(signature)
+
+    //json object send to MoMo endpoint
+    const requestBody = JSON.stringify({
+        partnerCode: partnerCode,
+        partnerName: "Test",
+        storeId: "MomoTestStore",
+        requestId: requestId,
+        amount: amount,
+        orderId: orderId,
+        orderInfo: orderInfo,
+        redirectUrl: redirectUrl,
+        ipnUrl: ipnUrl,
+        lang: lang,
+        requestType: requestType,
+        autoCapture: autoCapture,
+        extraData: extraData,
+        orderGroupId: orderGroupId,
+        signature: signature
+    });
+    //Create the HTTPS objects
+    const options = {
+        method: 'POST',
+        url: 'https://test-payment.momo.vn/v2/gateway/api/create',
+        headers: {
+            'Content-Type': 'application/json',
+            'Content-Length': Buffer.byteLength(requestBody)
+        },
+        data: requestBody
+    }
+    const result = await axios(options);
+    return res.status(200).json({
+        status: 'OK',
+        data: result.data
+    })
+}
+)
+router.post('/check_momo', async (req, res) => {
+    const { orderId } = req.body
+    const rawSignature = `accessKey=${accessKey}&orderId=${orderId}&partnerCode=MOMO&requestId=${orderId}`
+    const crypto = require('crypto')
+    var signature = crypto
+        .createHmac('sha256', secretKey)
+        .update(rawSignature)
+        .digest('hex');
+    const requestBody = JSON.stringify({
+        partnerCode: "MOMO",
+        requestId: orderId,
+        orderId,
+        signature,
+        lang: 'vi'
+    })
+    const options = {
+        method: 'POST',
+        url: 'https://test-payment.momo.vn/v2/gateway/api/query',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        data: requestBody
+    }
+    const result = await axios(options)
+    return res.status(200).json({
+        status: 'OK',
+        data: result.data
+    })
+}
+)
 
 module.exports = router
