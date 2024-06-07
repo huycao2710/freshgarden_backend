63dfe8e65cc7c176562c91ccf0eca4a458e44f2d
huycao2710 <caovanhuy2710@gmail.com>
2024-06-07 16:51:28 +0700

    vnpay

--- src/index.js
+++ src/index.js
@@ -11,7 +11,17 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3002
 
-app.use(cors())
+const corsOptions = {
+    origin: 'https://huycao2710.id.vn', // Thay đổi thành domain bạn cần cho phép
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    preflightContinue: false,
+    optionsSuccessStatus: 204
+};
+
+app.use(cors(corsOptions))
+app.options('*', cors(corsOptions));
+
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb' }))
 app.use(bodyParser.json())
