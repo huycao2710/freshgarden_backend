c549da475e1aace2fcb5c6a6f1e82f26b815f130
gumie <gumie3x1@gmail.com>
2024-06-07 17:52:14 +0700

    cors

--- src/index.js
+++ src/index.js
@@ -11,13 +11,13 @@ dotenv.config()
 const app = express()
 const port = process.env.PORT || 3002
 
-app.use(cors())
+app.use(cors({
+    origin: 'https://huycao2710.id.vn/'
+}))
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb' }))
 app.use(bodyParser.json())
 app.use(cookieParser())
-
-routes(app);
 
 mongoose.connect(`${process.env.MONGO_DB}`)
     .then(() => {
